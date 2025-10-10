c162db0f770b6e6280b0a68a1ce13df18c4e4068
LanderVennekens <lander.vennekens@gmail.com>
2025-10-10 20:36:32 +0100

    Creating Reservation page, viewmodel, controller

---
+++ Controllers/ReservationController.cs
@@ -0,0 +1,68 @@
+﻿using Restaurant.ViewModels.Reservation;
+
+namespace Restaurant.Controllers
+{
+    public class ReservationController : Controller
+    {
+        private readonly RestaurantContext _context;
+
+        public ReservationController(RestaurantContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Reservation/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var model = new ReservationViewModel
+            {
+                BeschikbareTijdsloten = _context.Tijdslots
+                    .Where(t => t.Actief)
+                    .Select(t => new TijdslotDto { Id = t.Id, Naam = t.Naam })
+                    .ToList()
+            };
+            return View(model);
+        }
+
+        // POST: /Reservation/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ReservationViewModel model)
+        {
+            // Herlaad tijdsloten bij fout
+            model.BeschikbareTijdsloten = _context.Tijdslots
+                .Where(t => t.Actief)
+                .Select(t => new TijdslotDto { Id = t.Id, Naam = t.Naam })
+                .ToList();
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // Maak een nieuwe Reservatie aan
+            var reservatie = new Reservatie
+            {
+                Datum = model.Datum,
+                TijdSlotId = model.TijdSlotId,
+                AantalPersonen = model.AantalPersonen,
+                Opmerking = model.Opmerking,
+                // KlantId moet hier gezet worden als de gebruiker is ingelogd
+                // Bijvoorbeeld: KlantId = User.FindFirstValue(ClaimTypes.NameIdentifier)
+            };
+
+            _context.Reservaties.Add(reservatie);
+            _context.SaveChanges();
+
+            return RedirectToAction("Confirmation");
+        }
+
+        // GET: /Reservation/Confirmation
+        [HttpGet]
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
+    }
+}
